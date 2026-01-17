21b3d1e42352a97da0183e80a0842e5d013d98ad
maryadesugba830-dotcom <maryadesugba830@gmail.com>
2026-01-17 06:35:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function openZendeskForm() {
   if (window.zE) {
     zE('webWidget', 'open');
     zE('webWidget', 'activate', 'contactForm');
-  } else {
-    alert("Support system is still loading. Please try again shortly.");
   }
 }
+
